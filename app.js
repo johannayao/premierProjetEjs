e03d6406c17c1bc9d35b7acffde44a94e715436a
yao johanna <yaojohanna2003@gmail.com>
2023-11-03 10:08:14 +0000

    my commit

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+
 const express = require("express")
+// const config = require("./config");
 const app = express()
-const require = ('ejs')
 const mongoose = require('mongoose')
 const bodyParser = require("body-parser")
 const cors = require('cors')
 const path = require('path')
 
+
+
+mongoose.connect('mongodb+srv://yaojohanna2003:johanna@cluster0.5mgufpk.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+ useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'))
+
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 app.use(cors())
 app.use(express.json())
-
+app.use(express.urlencoded({ extended: true }));
 module.exports = app
---
+++ config.js
@@ -0,0 +1,3 @@
+// module.exports = {
+//     mongoURI: "mongodb+srv://yaojohanna2003:johanna@cluster0.5mgufpk.mongodb.net/?retryWrites=true&w=majority"
+//   };
